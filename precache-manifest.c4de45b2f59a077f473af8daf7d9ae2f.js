30119af6fab541ee9c84a7027460094850ee7c76
Pascal Hanke <pascal.hanke@yahoo.de>
2020-01-05 15:48:10 +0100

    Updates

---
+++ precache-manifest.c4de45b2f59a077f473af8daf7d9ae2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9558f69a81c3f3cc615a6dff4731097b",
+    "url": "/shoppingbasket/index.html"
+  },
+  {
+    "revision": "f21b2c9efdce544bce9d",
+    "url": "/shoppingbasket/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d0c459e47b627ef18c48",
+    "url": "/shoppingbasket/static/js/2.1d8dc484.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shoppingbasket/static/js/2.1d8dc484.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f21b2c9efdce544bce9d",
+    "url": "/shoppingbasket/static/js/main.f05cc95f.chunk.js"
+  },
+  {
+    "revision": "4964528fa4ae528597af",
+    "url": "/shoppingbasket/static/js/runtime-main.938e9e08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppingbasket/static/media/logo.5d5d9eef.svg"
+  }
+]);
